d31b1aaf37ac7d5ed5ca88812fda700fec8bc734
robboten <robboten@gmail.com>
2022-11-14 22:53:57 +0100

    renames

--- LexNetGameR/Entity.cs
+++ LexNetGameR/Entity.cs
@@ -6,22 +6,16 @@ using System.Threading.Tasks;
 
 namespace LexNetGameR
 {
-    internal class Pawn
+    internal class Entity
     {
-        private Cell cell;
         public string Symbol { get; set; }
         public string Name { get; set; }    
         public ConsoleColor Color { get; set; } =ConsoleColor.Green;
-
-        public Cell Cell
+        public Vector2Int Position {get;set;}
+        public bool IsActive {get;set;}
+        public bool IsStatic {get;set;}
+        public Entity(string symbol, string name, ConsoleColor color)
         {
-            get => cell; 
-            set => cell = value;
-        }
-
-        public Pawn(Cell cell, string symbol, string name, ConsoleColor color)
-        {
-            this.cell = cell;
             Symbol = symbol;
             Name = name;
             Color = color;
--- LexNetGameR/EntityManager.cs
+++ LexNetGameR/EntityManager.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text; 
 using System.Threading.Tasks;
 
 namespace LexNetGameR
 {
-    internal class GameState
+    internal class EntityManager
     {
+        public Entity[,] entities;
     }
 }
